fd3d0bb656c0b47a497419b9901956c726bce56f
modeverv <modeverv@gmail.com>
2017-05-11 22:13:43 +0900

    initial

---
+++ backupkicker.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+PID=`ps x | grep "backuprsync" | grep -v grep | awk '{ print $1 }'`
+echo $PID
+if [ x"$PID" = x"" ]; then
+    echo "backupスタート"
+    /var/smb/backuprsync
+else
+    echo "backuprsyncは実行中のため終了します"
+fi
