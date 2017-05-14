76deed2706e22a3abe3ed504e09db14bf60bffd7
modeverv <modeverv@gmail.com>
2017-05-14 09:36:21 +0900

    path

--- backupkicker.sh
+++ backupkicker.sh
@@ -4,7 +4,7 @@ PID=`ps x | grep "backuprsync" | grep -v grep | awk '{ print $1 }'`
 echo $PID
 if [ x"$PID" = x"" ]; then
     echo "backupスタート"
-    /var/smb/backuprsync
+    /var/smb/backupscript/backuprsync
 else
     echo "backuprsyncは実行中のため終了します"
 fi
--- backupkickersystem.sh
+++ backupkickersystem.sh
@@ -3,7 +3,7 @@
 PID=`ps x | grep "backuprsystemsync" | grep -v grep | awk '{ print $1 }'`
 echo $PID
 if [ x"$PID" = x"" ]; then
-    /var/smb/backuprsystemsync
+    /var/smb/backupscript/backuprsystemsync
 else
     echo "backuprsystemsyncは実行中のため終了します"
 fi
